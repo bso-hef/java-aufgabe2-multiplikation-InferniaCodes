5e6aa3e364f6da9b0645eeccc69b1aea1d239d30
Sebastian Schmitt <se.schmitt@bso-hef.de>
2025-03-27 10:53:17 +0100

    Update Main.java
    Reverted

--- src/main/java/de/bsohef/Main.java
+++ src/main/java/de/bsohef/Main.java
@@ -1,25 +1,7 @@
-package org.example;
-import java.util.Scanner;
+package de.bsohef;
 
 public class Main {
     public static void main(String[] args) {
-
-        double ergebnis;
-        double zahl1;
-        double zahl2;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Bitte eine Zahl1 eingeben:");
-        zahl1 = input.nextDouble();
-
-        System.out.println("Bitte eine Zahl2 eingeben:");
-        zahl2 = input.nextDouble();
-
-        ergebnis = zahl1 + zahl2;
-        ergebnis = Math.floor(ergebnis * 100)/100;
-
-        System.out.println("Die Summe von " + zahl1 + " und " + zahl2 + " lautet: " + ergebnis);
-
+        System.out.println("Hello world!");
     }
 }
